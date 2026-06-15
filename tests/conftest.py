67a8376ef7b0175b387fb6f28599dedbf0c9bf81
Alex <alexander.floresb@pucp.pe>
2026-06-15 16:45:13 -0500

    Configuración inicial de DB para test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,61 @@
 from fastapi.testclient import TestClient   # Permite probars APIs sin levantar Uvicorn.
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from app.main import app
-from app.database import SessionLocal
+from app.database import Base
+from app.dependencies import get_db
 
 import pytest
 import uuid
-import random
+
+#region Configuración de DB de prueba
+
+TEST_DATABASE_URL = "sqlite:///./test.db"
+
+# Crea conexión SQLite
+test_engine = create_engine(
+    TEST_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+# Crea sesiones para consultas
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=test_engine
+)
+
+# Esto crea y cierra sesiones automáticamente por cada request
+def override_get_db():
+    db = TestingSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture
+def client():
+    app.dependency_overrides[get_db] = override_get_db
+    
+    with TestClient(app) as client:
+        yield client
+    
+    app.dependency_overrides.clear()    # Evita que el override quede activo para otros contextos
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_test_database():
+    Base.metadata.create_all(bind=test_engine)
+    yield
+    Base.metadata.drop_all(bind=test_engine)
+
+#endregion
 
 """
 Fixture sirve para preparar el entorno y los datos necesarios antes de ejecutar una prueba y limpiarlos después.
 Permite evitar código repetitivo y estructurar los tests de forma modular
 """
-@pytest.fixture
-def client():
-    return TestClient(app)
 
 @pytest.fixture
 def valid_user_payload():
@@ -71,6 +113,6 @@ def created_user(client, valid_user_payload):
 
 @pytest.fixture
 def db():
-    db = SessionLocal()
+    db = TestingSessionLocal()
     yield db
     db.close()
